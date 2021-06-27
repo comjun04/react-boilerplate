d672f3874d438c52a063f0a5998f2755fe93f66a
My Name <comjun04@gmail.com>
2021-06-27 13:03:44 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h3>Hello world</h3>
+      <p>Edit <code>src/App.jsx</code> and save to reload.</p>
+    </div>
+  )
+}
+
+export default App
