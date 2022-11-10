93cb14963bcf159817cee231fd1bafa3de1b0052
comjun04 <comjun04@gmail.com>
2022-11-11 00:35:11 +0900

    feat: enable jsx automatic runtime
    no need to use `import React from 'react'` anymore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import { render } from 'react-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx';
+import App from './App.jsx'
 
-render(<App />, document.getElementById('root'));
+const element = document.getElementById('root')
+const root = createRoot(element)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
 
--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,9 @@ module.exports = {
           options: {
             presets: [
               '@babel/preset-env',
-              '@babel/preset-react',
+              ['@babel/preset-react', {
+                runtime: 'automatic'
+              }]
             ]
           }
         },
